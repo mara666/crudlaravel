a3cc43c897db4231e5c00d4d31464850bb3c7120
Dario Marañes <dario.sjs@gmail.com>
2019-09-04 15:48:36 -0300

    Cambio la vista de Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -145,7 +145,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-block btn-primary">
+                                <button type="submit" class="btn btn-block btn-danger">
                                     {{ __('Registrarme') }}
                                 </button>
                             </div>
