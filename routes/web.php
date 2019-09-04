bfefeaf9cfbd71f371775dc80140f99360c6b791
Dario Marañes <dario.sjs@gmail.com>
2019-09-04 15:42:37 -0300

    Creo ruta para usuarios

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('usuarios', 'UsuariosController');
+
 Route::resource('libro', 'LibroController');
 
 Route::resource('autor', 'AutorController');
