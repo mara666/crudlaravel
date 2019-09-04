5692c1f07505cd75437fa4ce6cef3b0f9fcc91f6
Dario Marañes <dario.sjs@gmail.com>
2019-09-04 15:50:42 -0300

    Hago cambios estructurales en index de Libros

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -1,65 +1,67 @@
 @extends('layouts.master')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="pull-left">
-                        <h3>Listado de Libros</h3><br>
-                    </div>
-                    <div class="btn-group">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+    
+                @component('components.card')
+    
+                    @slot('header', 'Listado de Libros' )                        
+    
+                    @slot('body')
+                    <div class="row">
                         <a href="{{ route('libro.create') }}" class="btn btn-dark">Añadir un nuevo Libro</a>
                     </div>
-                    <br><br>
-                </div>
-                    <div class="table table-striped">
-                        <table id="mytable" class="table table-bordred table-striped">
-                            <thead class="thead-dark">
-                                <th>Nombre</th>
-                                <th>Resumen</th>
-                                <th>N° Páginas</th>
-                                <th>Edicion</th>
-                                <th>Autor</th>
-                                <th>Precio</th>
-                                <th>Acciones</th>
-                            </thead>
-                            <tbody>
-                                @forelse ($libros as $libro)
-                                    <tr>
-                                        <td>{{$libro->nombre}}</td>
-                                        <td>{{$libro->resumen}}</td>
-                                        <td>{{$libro->npagina}}</td>
-                                        <td>{{$libro->edicion}}</td>
-                                        <td>{{$libro->autor->nombre}}</td>
-                                        <td>{{$libro->precio}}</td>
-                                        <td>
-                                            <a href="{{ route('libro.show', $libro->id) }}"><i class="fas fa-eye"></i></a>
-                                        
-                                            <a href="{{ route('libro.edit', $libro->id) }}"><i class="fas fa-edit"></i></a>
-                                        
-                                            <form action="{{ route('libro.destroy', $libro->id)}}" method="post" class="d-inline">
-                                                @csrf
-                                                @method('DELETE')
 
-                                                <button type="submit"><i class="fas fa-trash"></i></button>
-                                        </td>                     
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="12">No hay registros cargados.</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-
-                        </table>
+                    <div class="row">
+                        <div class="table table-striped mt-2">
+                            <table id="mytable" class="table table-bordred table-striped">
+                                <thead class="thead-dark">
+                                    <th>Nombre</th>
+                                    <th>Resumen</th>
+                                    <th>N° Páginas</th>
+                                    <th>Edicion</th>
+                                    <th>Autor</th>
+                                    <th>Precio</th>
+                                    <th>Acciones</th>
+                                </thead>
+                                <tbody>
+                                    @forelse ($libros as $libro)
+                                        <tr>
+                                            <td>{{$libro->nombre}}</td>
+                                            <td>{{$libro->resumen}}</td>
+                                            <td>{{$libro->npagina}}</td>
+                                            <td>{{$libro->edicion}}</td>
+                                            <td>{{$libro->autor->nombre}}</td>
+                                            <td>{{$libro->precio}}</td>
+                                            <td>
+                                                <a href="{{ route('libro.show', $libro->id) }}"><u><i>Ver</i></u></a>
+                                                <a>/</a>
+                                                <a href="{{ route('libro.edit', $libro->id) }}"><u><i>Editar</i></u></a>
+                                                <a>/</a>
+                                                <form id="form_delete" action="{{ route('libro.destroy', $libro->id)}}" method="post" class="d-inline">
+                                                    @csrf
+                                                    @method('DELETE')
+    
+                                                <a href="{{ route('libro.destroy', $libro->id)}}" onclick="document.getElementById('form_delete').submit();; return false;"><u><i>Eliminar</i></u></a>
+                                            </td>                     
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="12">No hay registros cargados.</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>    
+                            </table>
+                        </div>                            
+                            {{ $libros->links() }}
                     </div>
-                </div>
-                {{ $libros->links() }}
+                    
+                    @endslot
+                @endcomponent
+    
             </div>
         </div>
     </div>
-
-</div>
 
 @endsection
